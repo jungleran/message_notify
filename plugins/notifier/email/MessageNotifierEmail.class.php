fa4d891739a0951614912c5d8c0a503b99f165ad
Amitai Burstein <amitai@gizra.com>
2012-07-26 00:47:23 +0300

    Add update function.

--- plugins/notifier/email/MessageNotifierEmail.class.php
+++ plugins/notifier/email/MessageNotifierEmail.class.php
@@ -11,7 +11,7 @@ class MessageNotifierEmail extends MessageNotifierBase {
   public static function viewModes() {
     return array(
       'message_notify_email_subject' => array('label' => t('Notify - Email subject')),
-      'message_notify_email_body' => array('label' => t('Notify - Email subject')),
+      'message_notify_email_body' => array('label' => t('Notify - Email body')),
     );
   }
 
@@ -31,6 +31,9 @@ class MessageNotifierEmail extends MessageNotifierBase {
     else {
       $lang = $languages[$message->language];
     }
+
+    // The subject in an email can't be with HTML, so strip it.
+    $output['message_notify_email_body'] = strip_tags($output['message_notify_email_body']);
 
     return drupal_mail('message_notify', $message->type, $mail, $lang, $output);
   }
