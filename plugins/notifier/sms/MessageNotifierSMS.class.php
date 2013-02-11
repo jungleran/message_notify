1b8c24f42c9914618fa172500d8e9aa0fa56e8e5
Amitaibu <amitai@gizra.com>
2013-02-11 14:55:57 +0200

    Fix view mode.

--- plugins/notifier/sms/MessageNotifierSMS.class.php
+++ plugins/notifier/sms/MessageNotifierSMS.class.php
@@ -7,6 +7,12 @@
 
 class MessageNotifierSMS {
 
+  public static function viewModes() {
+    return array(
+      'message_notify_sms_body' => array('label' => t('Notify - SMS Body')),
+    );
+  }
+
   public function deliver(array $output = array()) {
     if (!isset($this->message->smsNumber)) {
       throw new MessageNotifyException('Message cannot be sent using SMS as the "smsNumber" property is missing from the Message entity.');
